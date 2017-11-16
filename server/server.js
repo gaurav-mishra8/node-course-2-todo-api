f24efa28bc77c87b58ed9e7c618e0790c222a64d
gaurav1_mishra <gaurav1.mishra@paytm.com>
2017-11-16 20:57:38 +0530

    Add test for GET todos by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectId} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -33,6 +34,38 @@ app.get('/todos', (req, res) => {
     res.status(400).send(err);
   });
 });
+
+app.get('/todos/:id', (req, res) => {
+
+  var id = req.params.id;
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send({
+      status: false,
+      msg: 'Invalid Id'
+    });
+  }
+
+  Todo.findById(id).then((todo) => {
+  if (!todo) {
+    return res.status(404).send({
+      status: false,
+      msg: 'No results found'
+    });
+  }
+  res.send({status:'success',todo});
+  }, (e) => {
+  res.status(400).send({
+    status: false,
+    msg: 'Something went wrong'
+  });
+  }).catch((e) => {
+  res.status(400).send({
+    status: false,
+    msg: 'Something went wrong'
+  });
+  });
+
+  });
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
