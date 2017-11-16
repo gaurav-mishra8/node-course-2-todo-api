8bf8286cc925c63ecf971656619feae2188ca0d2
gaurav1_mishra <gaurav1.mishra@paytm.com>
2017-11-16 19:25:08 +0530

    Add test for GET /todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,8 +4,16 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+  text : 'First test Todo'
+}, {
+  text : 'Second test Todo'
+}];
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -27,7 +35,7 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -41,29 +49,43 @@ describe('POST /todos', () => {
 
   it('should not create todo with invalid body data', (done) => {
 
-  request(app)
-  .post('/todos')
-  .send({})
-  .expect(400)
-  .end((err, res) => {
+    request(app)
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .end((err, res) => {
 
-    if (err) {
-      return done(err);
-    }
+        if (err) {
+          return done(err);
+        }
 
-    Todo.find().then((todos) => {
-      expect(todos.length).toBe(0);
-      done();
-    }).catch((e) => {
-      done(e);
-    });
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(2);
+          done();
+        }).catch((e) => {
+          done(e);
+        });
+
+      });
+
+  }); // end of it should not create todo with invalid body data
+
+}); // end of describe block
+
+
+
+
+describe('GET /todos', () => {
+
+  it('should get all todos', (done) => {
+
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      }).end(done);
 
   });
 
-}); // end of it should not create todo with invalid body data
-
-
-
-
-
-}); // end of describe block
+});
