bb10e7f469c2f651cf40f711972acb6b4ccf1498
gaurav1_mishra <gaurav1.mishra@paytm.com>
2017-11-20 22:20:10 +0530

    Add GET users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ require('./config/config');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 
@@ -52,26 +53,29 @@ app.get('/todos/:id', (req, res) => {
   }
 
   Todo.findById(id).then((todo) => {
-  if (!todo) {
-    return res.status(404).send({
-      status: false,
-      msg: 'No results found'
+    if (!todo) {
+      return res.status(404).send({
+        status: false,
+        msg: 'No results found'
+      });
+    }
+    res.send({
+      status: 'success',
+      todo
     });
-  }
-  res.send({status:'success',todo});
   }, (e) => {
-  res.status(400).send({
-    status: false,
-    msg: 'Something went wrong'
-  });
+    res.status(400).send({
+      status: false,
+      msg: 'Something went wrong'
+    });
   }).catch((e) => {
-  res.status(400).send({
-    status: false,
-    msg: 'Something went wrong'
-  });
+    res.status(400).send({
+      status: false,
+      msg: 'Something went wrong'
+    });
   });
 
-  });
+});
 
 
 app.delete('/todos/:id', (req, res) => {
@@ -148,13 +152,19 @@ app.post('/users', (req, res) => {
   var user = new User(body);
 
   user.save().then(() => {
-     return user.generateAuthToken();
+    return user.generateAuthToken();
   }).then((token) => {
-    res.header('x-auth',token).send(user);
+    res.header('x-auth', token).send(user);
   }).catch((e) => {
     res.status(400).send(e);
   });
 
+});
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 
